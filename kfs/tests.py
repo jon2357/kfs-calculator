f38973b1cbad5fcb3837ebe4e412a91857058a3f
Jon <46353313+jon2357@users.noreply.github.com>
2022-07-20 11:34:23 -0400

    update

--- kfs/tests.py
+++ kfs/tests.py
@@ -17,5 +17,17 @@ def test_max_loan_amount_passed_in_modifier():
     value = max_loan_amount(arv, repairCosts,.9)
     assert value == 65000
     
-    
 
+#Test max_loan_amount
+def test_max_loan_request_default_modifier():
+    purchasePrice = 100000
+    repairCosts = 25000
+    value = max_loan_request(purchasePrice, repairCosts)
+    assert value == 115000
+
+# test max_loan_amount with a different arv modifier 
+def test_max_loan_request_passed_in_modifier():
+    purchasePrice = 100000
+    repairCosts = 25000
+    value = max_loan_request(purchasePrice, repairCosts,.8)
+    assert value == 105000
